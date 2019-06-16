cfd9bd325770748d979488b92d247490a479955a
vonbv25 <vonbv_25@yahoo.com.ph>
2019-06-16 18:23:23 +0800

    Merge the functionality of SequenceEventStream to EventStream

--- src/EventDrivers/EventStream.cs
+++ src/EventDrivers/EventStream.cs
@@ -6,7 +6,11 @@ namespace Kalev.Framework.Cqrs.EventSourcing.EventDrivers
     {
         private Guid _guid;
         private Guid _aggregateRootId;
+        private int _sequenceId;
+        private bool _startOfTheSequence;
+        private bool _endOfTheSequence;
         private DateTime _dateTimeCreated;
+        
         public EventStream(Guid aggregateRootId)
         {          
             _aggregateRootId    = aggregateRootId;
@@ -17,5 +21,26 @@ namespace Kalev.Framework.Cqrs.EventSourcing.EventDrivers
         public DateTime DateTimeCreated => _dateTimeCreated;
         public Guid Guid => _guid;
         public Guid AggregateRootId => _aggregateRootId;
+        public void SetSequenceId(int sequenceId)
+        {
+            _sequenceId = sequenceId;
+        }
+        public void SetThisEventStreamAsStartOfSequence()
+        {
+            _startOfTheSequence = true;
+        }
+        public void SetThisEventStreamAsEndOfSequence()
+        {
+            _endOfTheSequence = true;
+        }
+        public bool IsThisTheStartOfSequence()
+        {
+            return _startOfTheSequence == true;
+        }
+        public bool IsThisTheEndOfSequence()
+        {
+            return _endOfTheSequence == true;
+        }
+
     }
 }
